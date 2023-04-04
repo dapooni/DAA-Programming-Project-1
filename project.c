43792fcff24c96f8078ee1beaf532e9bf55040c2
Judith Balatinsayo <120451456+UditBal09@users.noreply.github.com>
2023-04-05 07:12:50 +0800

    Update project.c

--- project.c
+++ project.c
@@ -1,5 +1,34 @@
+
 #include<stdio.h>
+#include<stdlib.h>
+
+void selection_sort(int arr[], int size){
+	int temp, minValue, x , y;
+	
+	for(x = 0; x < size - 1; x++){
+		minValue = x;
+		for(y = x + 1; y < size; y++){
+			if(arr[y] < arr[minValue]){
+				minValue = y;
+			}
+		}
+		if(minValue != x){
+			temp = arr[minValue];
+			arr[minValue] = arr[x];
+			arr[x] = temp;
+		}
+	}
+}
 
 int main(){
-    //insert code here
+	int arr[] = {12, 91, 57, 32, 7, 9, 13};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int ctr;
+	
+	selection_sort(arr, size);
+	
+	printf("\nArray after sorting :\n");
+	for(ctr = 0; ctr < size; ctr++){
+		printf("%d ", arr[ctr]);
+	}
 }
